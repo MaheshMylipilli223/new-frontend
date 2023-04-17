6c4169742ee3484d0911adb1d4c168175e58b236
Mahesh <mahesh2312102@gmail.com>
2023-04-17 10:31:16 +0530

    first commit

---
+++ src/components/Leaderboard/Leaderboard.js
@@ -0,0 +1,39 @@
+import ProfileNavbar from "../ProfileNavbar/ProfileNavbar";
+import { useNavigate } from "react-router-dom";
+import BasicTable from "./BasicTable";
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
+
+function Leaderboard() {
+  const navigate = useNavigate()
+  if(! localStorage.getItem('user'))navigate('/login')
+  const [wordHuntRows, setWordHuntRows] = useState(null)
+  const [logicalRows, setLogicalRows] = useState(null)
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/logical/sort').then((response) => {
+      setLogicalRows(response.data.data)
+    })
+  }, [logicalRows])
+
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/wordhunt/sort').then((response) => {
+      setWordHuntRows(response.data.data)
+    })
+  }, [wordHuntRows])
+
+
+    return (
+      <>
+        <ProfileNavbar />
+        <div>
+          <BasicTable puzzle='WORD HUNT' rows={wordHuntRows}/>
+          <BasicTable puzzle='LOGICAL' rows={logicalRows} />
+        </div>
+      </>
+    );
+  }
+
+export default Leaderboard;
